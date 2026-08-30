b0ff8badb570f56400eba00edac72aaa0a5d858f
Phil Hunt <philiphuntiv2@gmail.com>
2026-08-30 08:42:33 -0400

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,38 @@
+param(
+    [String] $Src,
+    [String] $Dst,
+    [String[]] $Include
+)
+
+$Src = [System.IO.Path]::GetFullPath($Src)
+$Dst = [System.IO.Path]::GetFullPath($Dst)
+
+git.exe -C $PSScriptRoot submodule update --init --recursive --remote
+
+Push-Location "$PSScriptRoot\.msys2\ucrt64\bin"
+
+$Include += @(
+    "$PSScriptRoot\.python314\include"
+    "$PSScriptRoot\.pybind11\include"
+)
+
+& .\g++.exe -v `
+    -O3 -shared -std=c++17 -fPIC `
+    @($Include | ForEach-Object { "-I'$_'" }) `
+    $Src `
+    -o $Dst `
+    -L"$python314\libs" `
+    -lpython314 `
+    -lsetupapi `
+    -lcfgmgr32
+
+$py = "$PSScriptRoot\.python314\python.exe"
+
+& $py -m pip pybind11-stubgen
+
+& $py -m pybind11_stubgen `
+    [System.IO.Path]::GetFileNameWithoutExtension($Dst) `
+    --output-dir [System.IO.Path]::GetDirectoryName($Dst)
+
+Pop-Location
+
